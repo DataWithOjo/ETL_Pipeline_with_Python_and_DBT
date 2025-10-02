d4c30690493b0174484a40a97fd3f87b51b18a83
ojo <ojokayode13@gmail.com>
2025-10-02 13:29:59 +0100

    Project progress after adding gitignore, config.py, etl_pipline.py, requirements.txt

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Database connection details
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+database_url = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+source_url = "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
+source_folder = "Raw"
+schema_name = "dev"
+table_name = "finance_2023"
---
+++ etl_pipeline.py
@@ -0,0 +1,83 @@
+import os
+import requests
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+from config import database_url, source_url, schema_name, source_folder, table_name
+
+
+def get_source_data():
+    # Downloads the CSV file from the source link. Saves it to the Raw folder.
+
+    print("Starting download process")
+    # Create Folder "Raw" if not exists
+    os.makedirs(source_folder, exist_ok=True)
+    print(f"Created Destination folder {source_folder}")
+    
+    file_path = os.path.join(source_folder, source_url.split("/")[-1])
+
+    try:
+        response = requests.get(source_url, stream=True)
+        response.raise_for_status()
+        
+        with open(file_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"Download successful: '{file_path}' created")
+        return file_path
+    except Exception as e:
+        print(f"Download failed: {e}")
+        return None
+    
+def convert_data(file_path):
+    # Reads the csv file from folder "Raw" into pandas Dataframe. Performs transformation
+    
+    print("Starting Conversion")
+    try:
+        finance_data = pd.read_csv(file_path)
+        print("Successfully read the csv data")
+        return finance_data
+    
+    except FileNotFoundError:
+        print(f"Conversion failed: {file_path} not found")
+        return None
+
+def load_data(finance_data: pd.DataFrame, conn_string: str, table_name: str, schema_name: str):
+    # Loads the Dataframe into our PostgresSQL database table.
+    
+    print(f"Start loading data into PostgreSQL table '{schema_name}.{table_name}'")
+    
+    try:
+        engine = create_engine(conn_string)
+        
+        with engine.connect() as conn:
+            conn.execute(text(f'CREATE SCHEMA IF NOT EXISTS {schema_name}'))
+            conn.commit()
+            print(f"Schema '{schema_name}' created successfully.")
+            
+            finance_data.to_sql(
+                name=table_name,
+                con=conn,
+                schema=schema_name,
+                if_exists="replace",
+                index=False
+            )
+        print(f"Loading successful: Data written to '{schema_name}.{table_name}'.")
+    
+    except Exception as e:
+        print(f"Loading failed: {e}")
+        
+            
+def run_etl_pipeline():
+    # Executes the full ETL pipeline.
+   
+    file_path = get_source_data()
+    
+    if file_path:
+        finance_data = convert_data(file_path)
+        
+        if finance_data is not None:
+            load_data(finance_data, database_url, table_name, schema_name)
+        
+if __name__ == "__main__":
+    run_etl_pipeline()
